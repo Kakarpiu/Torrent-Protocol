b157d8d56dba7c2c117073ce811999776716ed49
karpiesi <karpiesi@KARPIESI1.emea.hpqcorp.net>
2016-12-13 15:49:33 +0100

    Connection class begining need to think of protocol

--- Torrent/src/Connection.java
+++ Torrent/src/Connection.java
@@ -1,3 +1,5 @@
+import java.io.*;
+import java.net.*;
 
 public class Connection {
 	
@@ -5,9 +7,18 @@ public class Connection {
 	private static String ip;
 	private static int port;
 	private static String protocol;
+	private boolean lock = false;
+	
+	// TCP protocol vars
+	private Socket peerSocket = null;
+	private PrintWriter TCP_out = null;
+	private BufferedReader TCP_input = null;
+	
+	// UDP protocol vars
+	private DatagramSocket peerSocker = null;
+	
 	private static Connection instance = null;
 	// FILETRANSFER LIST
-	
 	
 	protected Connection(String ip, int port, String protocol)
 	{
@@ -19,12 +30,7 @@ public class Connection {
 	public Connection getInstance(String ip, int port, String protocol)
 	{
 		if(instance == null)
-		{
-			this.ip = ip;
-			this.port = port;
-			this.protocol = protocol;
 			instance = new Connection(ip, port, protocol);
-		}
 		return instance;
 	}
 	
@@ -43,5 +49,38 @@ public class Connection {
 		return protocol;
 	}
 	
-	
+	public void connect()
+	{
+		if(protocol.equals("TCP") && lock == false)
+		{
+			try 
+			{
+				peerSocket = new Socket(ip, port);
+				TCP_out = new PrintWriter(peerSocket.getOutputStream(), true);
+				TCP_input = new BufferedReader(
+						new InputStreamReader(peerSocket.getInputStream()));
+				
+				String handshake;
+				TCP_out.println("Connect");
+				while((handshake = TCP_input.readLine()) != null)
+				{
+					if(handshake.equals("ACK"))
+						lock = true;
+				}
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
 }
+
+
+
+
+
+
+
+
+
--- Torrent/src/UserInterface.java
+++ Torrent/src/UserInterface.java
@@ -48,32 +48,33 @@ public class UserInterface implements Runnable{
 		
 		switch(commandName){
 			
-			case "CONN" :  
-			{
-				if (argsCount != 4)
-				{
-					System.out.println("Wrong number of arguments. This command takes 3 arguments in form of. \nIP PORT PROTOCOL ");
-					return;
-				}
-				String ip = arguments[1];;
-				int port;
-				String protocol;
-				
-				try
-				{
-					port = Integer.parseInt(arguments[2]);
-				} catch (Exception e) { System.out.println("Port number is not Integer!"); return; };
-				try
-				{
-					protocol = arguments[3];
-				} catch (Exception e) { System.out.println("Protocol doesn't match. Please choose TCP or UDP. TCP is default"); return;}
-				
-				
-				peer = new Connection(ip, port, protocol);
-				
-				
-			}
+			case "CONN" :  CONN(arguments, argsCount); break;
 		}
+	}
+	
+	public String CONN(String[] arguments, int argsCount)
+	{
+		if (argsCount != 4)
+		{
+			return "Wrong number of arguments. This command takes 3 arguments in form of. \nIP PORT PROTOCOL ";
+		}
+		String ip = arguments[1];;
+		int port;
+		String protocol;
+		
+		try
+		{
+			port = Integer.parseInt(arguments[2]);
+		} catch (Exception e) { return "Port number is not Integer!"; };
+		try
+		{
+			protocol = arguments[3];
+		} catch (Exception e) { return "Protocol doesn't match. Please choose TCP or UDP. TCP is default";}
+		
+		peer = new Connection(ip, port, protocol);
+		peer.connect();
+		
+		return "Connected.";
 	}
 	
 }
